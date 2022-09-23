0f778919a8afbd4c96172a1af9896a92d6cd0c94
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-09-23 13:36:10 +0600

    add all manually & conditionally

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -7,31 +7,52 @@ const User = (props) => {
     // normal system
     let greetings;
     if (familiar) {
-        greetings = <p>Welcome, my friend.</p>    
+        greetings = <p>Welcome, my friend.</p>
     } else {
         greetings = <p>Who the hell are you?</p>
     }
 
+    // JavaScript ternary operator
+    // const greeting = familiar ? 'hello' : 'bye';
+    /** const food = familiar
+        ? <p>I will buy Food for you.</p>
+        : <p>Lets eat his his whose whose</p>
+    */
+
+    // jodi 2ta condition 1sathei fullfil korte hobe, tobe
+    // logical &&
+    /* const connection = familiar && <p>Let's join my facebook</p> */
+
     return (
         <>
-          <div>
-            <h2>Greetings</h2>
-            {greetings}
-          </div>
+            <div>
+                <h2>Greetings</h2>
+                {greetings}
+            </div>
 
 
-          <div>
-            <h2>Food</h2>
-            <p>I will buy Food foe you.</p>
-            <p>Lets eat his his whose whose</p>
-          </div>
-          
-          
-          <div>
-            <h2>Connection</h2>
-            <p>Let's join my facebook</p>
-            <p>I don't add stranger.</p>
-          </div>
+            <div>
+                <h2>Food</h2>
+                {/* {food} */}
+
+                {
+                    familiar
+                        ? <p>I will buy Food for you.</p>
+                        : <p>Lets eat his his whose whose</p>
+                }
+            </div>
+
+
+            <div>
+                <h2>Connection</h2>
+                {/* {connection} */}
+                {/* <p>I don't add stranger.</p> */}
+
+                {
+                    familiar && <p>Let's join my facebook</p>
+                }
+
+            </div>
 
         </>
     );
